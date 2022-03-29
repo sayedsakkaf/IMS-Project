8fc08a3bc88fe2184f5cdda0b9d3e26aee78e13f
Sayed <sayedsakkaf1@hotmail.com>
2022-03-29 15:27:41 +0100

    Updated testing to include Item Controller Testing

---
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -0,0 +1,66 @@
+package com.qa.ims.controller;
+
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+
+public class ItemController implements CrudController<Item> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+	
+	private ItemDAO itemDAO;
+	private Utils utils;
+	
+	public ItemController(ItemDAO itemDAO, Utils utils) {
+		super();
+		this.itemDAO = itemDAO;
+		this.utils = utils;
+	}
+
+	@Override
+	public List<Item> readAll() {
+		List<Item> items = itemDAO.readAll();
+		for (Item item : items) {
+			LOGGER.info(item);
+		}
+		return items;
+	}
+
+	@Override
+	public Item create() {
+		LOGGER.info("Please enter a product name");
+		String productName = utils.getString();
+		LOGGER.info("Please enter a price");
+		double price = utils.getDouble();
+		Item item = itemDAO.create(new Item(productName, price));
+		LOGGER.info("Item created!");
+		return item;
+	}
+
+	@Override
+	public Item update() {
+		LOGGER.info("Please enter the id of the item you would like to update");
+		Long id = utils.getLong();
+		LOGGER.info("Please enter a product name");
+		String productName = utils.getString();
+		LOGGER.info("Please enter a price");
+		double price = utils.getDouble();
+		Item item = itemDAO.update(new Item(id, productName, price));
+		LOGGER.info("Item updated!");
+		return item;
+		}
+
+	@Override
+	public int delete() {
+		LOGGER.info("Please enter the id of the item you would like to delete");
+		Long id = utils.getLong();
+		return itemDAO.delete(id);
+	}
+
+
+}
