e376233bb5b1676703d9ad6e8d822373cf886063
Sayed <sayedsakkaf1@hotmail.com>
2022-03-30 15:11:43 +0100

    IMS-9 #comment Added Order and OrderDAO

---
+++ src/main/java/com/qa/ims/persistence/domain/Order.java
@@ -0,0 +1,54 @@
+package com.qa.ims.persistence.domain;
+
+import java.util.Objects;
+
+public class Order {
+	
+	private Long id;
+	private Long customerId;
+	
+	public Order(Long id, Long customerId) {
+		this.setId(id);
+		this.setCustomerId(customerId);
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Long customerId) {
+		this.customerId = customerId;
+	}
+
+	@Override
+	public String toString() {
+		return "Orders [id=" + id + ", customerId=" + customerId + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(customerId, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(customerId, other.customerId) && Objects.equals(id, other.id);
+	}
+	
+	
+}
